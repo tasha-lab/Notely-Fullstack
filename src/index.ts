13821d606c06fb6c959c29c78d99c38db7e05787
Halima <natashawanjiku31@gmail.com>
2025-07-28 12:43:17 -0400

    add live url

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://notely-fullstack.vercel.app"],
     methods: ["POST", "GET", "PATCH", "PUT", "DELETE"],
   })
 );
